e973a204641dad45610ab31de2c0f6138223582d
Людмила Алтухова <luda.tamb@mail.ru>
2022-10-14 13:45:00 +0300

    feat: implementation card list

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,3 +1,26 @@
-const CardList = () => {};
+import styled from "styled-components";
+
+import Card from "./Card";
+
+const StyledCardList = styled.ul`
+  list-style: none;
+  margin-top: 30px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 370px);
+  grid-gap: 1rem;
+  justify-content: space-between;
+`;
+
+const CardList = (data) => {
+  const cardList = data.data.map((item) => (
+    <Card
+      key={item.name}
+      title={item.name}
+      phone={item.phone}
+      email={item.email}
+    />
+  ));
+  return <StyledCardList>{cardList}</StyledCardList>;
+};
 
 export default CardList;
