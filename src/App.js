3bb36361b7328cce598ea2423fa127b78c4cc519
Людмила Алтухова <luda.tamb@mail.ru>
2022-10-15 15:21:00 +0300

    feat: implement main component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,67 @@
 import { useState, useEffect } from "react";
+import styled from "styled-components";
 
+import Context from "./context";
 import CardList from "./components/CardList";
+import SearchBar from "./components/SearchBar";
+import Modal from "./components/Modal";
 import { getData } from "./services/getData";
+
+const AppContainer = styled.div`
+  max-width: 1200px;
+  margin: 20px auto;
+`;
 
 const App = () => {
   const [appData, setAppData] = useState([]);
   const [isLoadingData, setIsLoadingData] = useState(false);
 
-  useEffect(() => {
-    setIsLoadingData(true);
+  const [filteredData, setFilteredData] = useState([]);
 
+  const [isModalShow, setIsModalShow] = useState(false);
+  const [currentUser, setCurrentUser] = useState({});
+
+  const renderCards = () => {
+    setIsLoadingData(true);
     getData().then((items) => {
-      if (isLoadingData) {
-        setAppData(items);
-      }
+      setAppData(items);
+      setFilteredData(items);
     });
     setIsLoadingData(false);
-  }, [isLoadingData]);
+  };
+
+  const filterData = (searchFieldValue) => {
+    const filteredAppData = appData.filter((item) => {
+      return item.name.toLowerCase().includes(searchFieldValue.toLowerCase());
+    });
+
+    setFilteredData(filteredAppData);
+  };
+
+  useEffect(() => {
+    renderCards();
+  }, []);
+
+  const showModal = (isShow, title) => {
+    setIsModalShow(isShow);
+
+    setCurrentUser(appData.find((item) => item.name === title));
+  };
 
   return (
-    <>
-      <input />
-      {isLoadingData && <div>A moment please...</div>}
-      <CardList data={appData} />
-    </>
+    <Context.Provider value={{ showModal }}>
+      <>
+        <AppContainer>
+          <SearchBar filterData={filterData} />
+
+          {isLoadingData && <div>A moment please...</div>}
+          {!isLoadingData && <CardList data={filteredData} />}
+          {isModalShow && (
+            <Modal setIsModalShow={setIsModalShow} currentUser={currentUser} />
+          )}
+        </AppContainer>
+      </>
+    </Context.Provider>
   );
 };
 
