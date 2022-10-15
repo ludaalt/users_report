3c30070328c91abd26682a22dee57b0d348abdf0
Людмила Алтухова <luda.tamb@mail.ru>
2022-10-15 15:47:13 +0300

    refactor: add server data replacement

--- src/services/getData.js
+++ src/services/getData.js
@@ -1,5 +1,12 @@
+import users from "../users.json";
+
 export const getData = async () => {
-  const response = await fetch(process.env.REACT_APP_BASE_URL);
-  const data = await response.clone().json();
-  return data;
+  if (process.env.REACT_APP_BASE_URL) {
+    const response = await fetch(process.env.REACT_APP_BASE_URL);
+    const data = await response.clone().json();
+
+    return data;
+  } else {
+    return users;
+  }
 };
